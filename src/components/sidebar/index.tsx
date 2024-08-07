906f6daccaaec7f23afe83b059478c9afa50ffe4
Marko Melle <markmelle1@gmail.com>
2024-08-07 09:16:33 +0200

    modified Sidebar and Home components

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,29 +1,29 @@
-import { useLocation } from "react-router-dom";
-import BookmarkIcon from "../icons/bookmark-icon";
-import HomeIcon from "../icons/home-icon";
-import MovieIcon from "../icons/movie-icon";
-import TvSeriesIcon from "../icons/series-icon";
-import { Box } from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
+import homeIcon from "../../assets/icons/icon-nav-home.svg";
+import movieIcon from "../../assets/icons/icon-nav-movies.svg";
+import tvSeriesIcon from "../../assets/icons/icon-nav-tv-series.svg";
+import bookmarkIcon from "../../assets/icons/icon-nav-bookmark.svg";
+import { Box, Typography, MenuItem } from "@mui/material";
 
 const navLinks = [
   {
     name: "Home",
-    icon: HomeIcon,
+    icon: homeIcon,
     link: "/home",
   },
   {
     name: "Movies",
-    icon: MovieIcon,
+    icon: movieIcon,
     link: "/movies",
   },
   {
     name: "Bookmark",
-    icon: BookmarkIcon,
+    icon: bookmarkIcon,
     link: "/bookmark",
   },
   {
     name: "Tv series",
-    icon: TvSeriesIcon,
+    icon: tvSeriesIcon,
     link: "/tv-series",
   },
 ];
@@ -49,6 +49,92 @@ const Sidebar = () => {
         },
       }}
     >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: {
+            xs: "row",
+            lg: "column",
+          },
+          gap: 5,
+          alignItems: {
+            xs: "center",
+            lg: "start",
+          },
+          width: "100%",
+        }}
+      >
+        <Typography
+          variant="h5"
+          component="h1"
+          my={2}
+          fontWeight={400}
+          fontSize={18}
+          sx={{
+            display: {
+              xs: "none",
+              sm: "block",
+            },
+          }}
+        >
+          Video App
+        </Typography>
+        <Box
+          sx={{
+            py: {
+              xs: "0px",
+              lg: "16px",
+            },
+            display: "flex",
+            flexDirection: {
+              xs: "row",
+              lg: "column",
+            },
+            gap: 4,
+          }}
+        >
+          {navLinks.map((item) => (
+            <Link
+              key={item.name}
+              to={item.link}
+              style={{ textDecoration: "none" }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 2,
+                  color: "white",
+                  textDecoration: "none",
+                }}
+              >
+                <img
+                  src={item.icon}
+                  alt={item.name}
+                  style={{
+                    width: "18px",
+                    filter: `${
+                      pathname === item.link
+                        ? "invert(58%) sepia(14%) saturate(3166%) hue-rotate(215deg) brightness(91%) contrast(87%)"
+                        : "invert(84%)"
+                    }`,
+                  }}
+                ></img>
+                <Typography
+                  sx={{
+                    display: {
+                      xs: "none",
+                      md: "block",
+                    },
+                  }}
+                >
+                  {item.name}
+                </Typography>
+              </Box>
+            </Link>
+          ))}
+        </Box>
+      </Box>
     </Box>
   );
 };
