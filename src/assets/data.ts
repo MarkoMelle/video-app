36b4e3bfe3f7ab5f607c811f7680bd167b5c04ed
Marko Melle <markmelle1@gmail.com>
2024-08-18 19:40:32 +0200

    modified:   src/assets/data.ts

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -25,13 +25,13 @@ export const moviesData = [
     title: "Beyond Earth",
     thumbnail: {
       trending: {
-        small: "../thumbnails/beyond-earth/trending/small.jpg",
-        large: "../thumbnails/beyond-earth/trending/large.jpg",
+        small: "../../thumbnails/beyond-earth/trending/small.jpg",
+        large: "../../thumbnails/beyond-earth/trending/large.jpg",
       },
       regular: {
-        small: "../thumbnails/beyond-earth/regular/small.jpg",
-        medium: "../thumbnails/beyond-earth/regular/medium.jpg",
-        large: "../thumbnails/beyond-earth/regular/large.jpg",
+        small: "../../thumbnails/beyond-earth/regular/small.jpg",
+        medium: "../../thumbnails/beyond-earth/regular/medium.jpg",
+        large: "../../thumbnails/beyond-earth/regular/large.jpg",
       },
     },
     year: 2019,
@@ -45,13 +45,13 @@ export const moviesData = [
     title: "Bottom Gear",
     thumbnail: {
       trending: {
-        small: "../thumbnails/bottom-gear/trending/small.jpg",
-        large: "../thumbnails/bottom-gear/trending/large.jpg",
+        small: "../../thumbnails/bottom-gear/trending/small.jpg",
+        large: "../../thumbnails/bottom-gear/trending/large.jpg",
       },
       regular: {
-        small: "../thumbnails/bottom-gear/regular/small.jpg",
-        medium: "../thumbnails/bottom-gear/regular/medium.jpg",
-        large: "../thumbnails/bottom-gear/regular/large.jpg",
+        small: "../../thumbnails/bottom-gear/regular/small.jpg",
+        medium: "../../thumbnails/bottom-gear/regular/medium.jpg",
+        large: "../../thumbnails/bottom-gear/regular/large.jpg",
       },
     },
     year: 2021,
@@ -65,13 +65,13 @@ export const moviesData = [
     title: "Undiscovered Cities",
     thumbnail: {
       trending: {
-        small: "../thumbnails/undiscovered-cities/trending/small.jpg",
-        large: "../thumbnails/undiscovered-cities/trending/large.jpg",
+        small: "../../thumbnails/undiscovered-cities/trending/small.jpg",
+        large: "../../thumbnails/undiscovered-cities/trending/large.jpg",
       },
       regular: {
-        small: "../thumbnails/undiscovered-cities/regular/small.jpg",
-        medium: "../thumbnails/undiscovered-cities/regular/medium.jpg",
-        large: "../thumbnails/undiscovered-cities/regular/large.jpg",
+        small: "../../thumbnails/undiscovered-cities/regular/small.jpg",
+        medium: "../../thumbnails/undiscovered-cities/regular/medium.jpg",
+        large: "../../thumbnails/undiscovered-cities/regular/large.jpg",
       },
     },
     year: 2019,
@@ -85,13 +85,13 @@ export const moviesData = [
     title: "1998",
     thumbnail: {
       trending: {
-        small: "../thumbnails/1998/trending/small.jpg",
-        large: "../thumbnails/1998/trending/large.jpg",
+        small: "../../thumbnails/1998/trending/small.jpg",
+        large: "../../thumbnails/1998/trending/large.jpg",
       },
       regular: {
-        small: "../thumbnails/1998/regular/small.jpg",
-        medium: "../thumbnails/1998/regular/medium.jpg",
-        large: "../thumbnails/1998/regular/large.jpg",
+        small: "../../thumbnails/1998/regular/small.jpg",
+        medium: "../../thumbnails/1998/regular/medium.jpg",
+        large: "../../thumbnails/1998/regular/large.jpg",
       },
     },
     year: 2021,
@@ -105,13 +105,13 @@ export const moviesData = [
     title: "Dark Side of the Moon",
     thumbnail: {
       trending: {
-        small: "../thumbnails/dark-side-of-the-moon/trending/small.jpg",
-        large: "../thumbnails/dark-side-of-the-moon/trending/large.jpg",
+        small: "../../thumbnails/dark-side-of-the-moon/trending/small.jpg",
+        large: "../../thumbnails/dark-side-of-the-moon/trending/large.jpg",
       },
       regular: {
-        small: "../thumbnails/dark-side-of-the-moon/regular/small.jpg",
-        medium: "../thumbnails/dark-side-of-the-moon/regular/medium.jpg",
-        large: "../thumbnails/dark-side-of-the-moon/regular/large.jpg",
+        small: "../../thumbnails/dark-side-of-the-moon/regular/small.jpg",
+        medium: "../../thumbnails/dark-side-of-the-moon/regular/medium.jpg",
+        large: "../../thumbnails/dark-side-of-the-moon/regular/large.jpg",
       },
     },
     year: 2018,
@@ -125,9 +125,9 @@ export const moviesData = [
     title: "The Great Lands",
     thumbnail: {
       regular: {
-        small: "../thumbnails/the-great-lands/regular/small.jpg",
-        medium: "../thumbnails/the-great-lands/regular/medium.jpg",
-        large: "../thumbnails/the-great-lands/regular/large.jpg",
+        small: "../../thumbnails/the-great-lands/regular/small.jpg",
+        medium: "../../thumbnails/the-great-lands/regular/medium.jpg",
+        large: "../../thumbnails/the-great-lands/regular/large.jpg",
       },
     },
     year: 2019,
@@ -141,9 +141,9 @@ export const moviesData = [
     title: "The Diary",
     thumbnail: {
       regular: {
-        small: "../thumbnails/the-diary/regular/small.jpg",
-        medium: "../thumbnails/the-diary/regular/medium.jpg",
-        large: "../thumbnails/the-diary/regular/large.jpg",
+        small: "../../thumbnails/the-diary/regular/small.jpg",
+        medium: "../../thumbnails/the-diary/regular/medium.jpg",
+        large: "../../thumbnails/the-diary/regular/large.jpg",
       },
     },
     year: 2019,
@@ -157,9 +157,9 @@ export const moviesData = [
     title: "Earth’s Untouched",
     thumbnail: {
       regular: {
-        small: "../thumbnails/earths-untouched/regular/small.jpg",
-        medium: "../thumbnails/earths-untouched/regular/medium.jpg",
-        large: "../thumbnails/earths-untouched/regular/large.jpg",
+        small: "../../thumbnails/earths-untouched/regular/small.jpg",
+        medium: "../../thumbnails/earths-untouched/regular/medium.jpg",
+        large: "../../thumbnails/earths-untouched/regular/large.jpg",
       },
     },
     year: 2017,
@@ -173,9 +173,9 @@ export const moviesData = [
     title: "No Land Beyond",
     thumbnail: {
       regular: {
-        small: "../thumbnails/no-land-beyond/regular/small.jpg",
-        medium: "../thumbnails/no-land-beyond/regular/medium.jpg",
-        large: "../thumbnails/no-land-beyond/regular/large.jpg",
+        small: "../../thumbnails/no-land-beyond/regular/small.jpg",
+        medium: "../../thumbnails/no-land-beyond/regular/medium.jpg",
+        large: "../../thumbnails/no-land-beyond/regular/large.jpg",
       },
     },
     year: 2019,
@@ -189,9 +189,9 @@ export const moviesData = [
     title: "During the Hunt",
     thumbnail: {
       regular: {
-        small: "../thumbnails/during-the-hunt/regular/small.jpg",
-        medium: "../thumbnails/during-the-hunt/regular/medium.jpg",
-        large: "../thumbnails/during-the-hunt/regular/large.jpg",
+        small: "../../thumbnails/during-the-hunt/regular/small.jpg",
+        medium: "../../thumbnails/during-the-hunt/regular/medium.jpg",
+        large: "../../thumbnails/during-the-hunt/regular/large.jpg",
       },
     },
     year: 2016,
@@ -205,9 +205,9 @@ export const moviesData = [
     title: "Autosport the Series",
     thumbnail: {
       regular: {
-        small: "../thumbnails/autosport-the-series/regular/small.jpg",
-        medium: "../thumbnails/autosport-the-series/regular/medium.jpg",
-        large: "../thumbnails/autosport-the-series/regular/large.jpg",
+        small: "../../thumbnails/autosport-the-series/regular/small.jpg",
+        medium: "../../thumbnails/autosport-the-series/regular/medium.jpg",
+        large: "../../thumbnails/autosport-the-series/regular/large.jpg",
       },
     },
     year: 2016,
@@ -221,9 +221,9 @@ export const moviesData = [
     title: "Same Answer II",
     thumbnail: {
       regular: {
-        small: "../thumbnails/same-answer-2/regular/small.jpg",
-        medium: "../thumbnails/same-answer-2/regular/medium.jpg",
-        large: "../thumbnails/same-answer-2/regular/large.jpg",
+        small: "../../thumbnails/same-answer-2/regular/small.jpg",
+        medium: "../../thumbnails/same-answer-2/regular/medium.jpg",
+        large: "../../thumbnails/same-answer-2/regular/large.jpg",
       },
     },
     year: 2017,
@@ -237,9 +237,9 @@ export const moviesData = [
     title: "Below Echo",
     thumbnail: {
       regular: {
-        small: "../thumbnails/below-echo/regular/small.jpg",
-        medium: "../thumbnails/below-echo/regular/medium.jpg",
-        large: "../thumbnails/below-echo/regular/large.jpg",
+        small: "../../thumbnails/below-echo/regular/small.jpg",
+        medium: "../../thumbnails/below-echo/regular/medium.jpg",
+        large: "../../thumbnails/below-echo/regular/large.jpg",
       },
     },
     year: 2016,
@@ -253,9 +253,9 @@ export const moviesData = [
     title: "The Rockies",
     thumbnail: {
       regular: {
-        small: "../thumbnails/the-rockies/regular/small.jpg",
-        medium: "../thumbnails/the-rockies/regular/medium.jpg",
-        large: "../thumbnails/the-rockies/regular/large.jpg",
+        small: "../../thumbnails/the-rockies/regular/small.jpg",
+        medium: "../../thumbnails/the-rockies/regular/medium.jpg",
+        large: "../../thumbnails/the-rockies/regular/large.jpg",
       },
     },
     year: 2015,
@@ -269,9 +269,9 @@ export const moviesData = [
     title: "Relentless",
     thumbnail: {
       regular: {
-        small: "../thumbnails/relentless/regular/small.jpg",
-        medium: "../thumbnails/relentless/regular/medium.jpg",
-        large: "../thumbnails/relentless/regular/large.jpg",
+        small: "../../thumbnails/relentless/regular/small.jpg",
+        medium: "../../thumbnails/relentless/regular/medium.jpg",
+        large: "../../thumbnails/relentless/regular/large.jpg",
       },
     },
     year: 2017,
@@ -285,9 +285,9 @@ export const moviesData = [
     title: "Community of Ours",
     thumbnail: {
       regular: {
-        small: "../thumbnails/community-of-ours/regular/small.jpg",
-        medium: "../thumbnails/community-of-ours/regular/medium.jpg",
-        large: "../thumbnails/community-of-ours/regular/large.jpg",
+        small: "../../thumbnails/community-of-ours/regular/small.jpg",
+        medium: "../../thumbnails/community-of-ours/regular/medium.jpg",
+        large: "../../thumbnails/community-of-ours/regular/large.jpg",
       },
     },
     year: 2018,
@@ -301,9 +301,9 @@ export const moviesData = [
     title: "Van Life",
     thumbnail: {
       regular: {
-        small: "../thumbnails/van-life/regular/small.jpg",
-        medium: "../thumbnails/van-life/regular/medium.jpg",
-        large: "../thumbnails/van-life/regular/large.jpg",
+        small: "../../thumbnails/van-life/regular/small.jpg",
+        medium: "../../thumbnails/van-life/regular/medium.jpg",
+        large: "../../thumbnails/van-life/regular/large.jpg",
       },
     },
     year: 2015,
@@ -317,9 +317,9 @@ export const moviesData = [
     title: "The Heiress",
     thumbnail: {
       regular: {
-        small: "../thumbnails/the-heiress/regular/small.jpg",
-        medium: "../thumbnails/the-heiress/regular/medium.jpg",
-        large: "../thumbnails/the-heiress/regular/large.jpg",
+        small: "../../thumbnails/the-heiress/regular/small.jpg",
+        medium: "../../thumbnails/the-heiress/regular/medium.jpg",
+        large: "../../thumbnails/the-heiress/regular/large.jpg",
       },
     },
     year: 2021,
@@ -333,9 +333,9 @@ export const moviesData = [
     title: "Off the Track",
     thumbnail: {
       regular: {
-        small: "../thumbnails/off-the-track/regular/small.jpg",
-        medium: "../thumbnails/off-the-track/regular/medium.jpg",
-        large: "../thumbnails/off-the-track/regular/large.jpg",
+        small: "../../thumbnails/off-the-track/regular/small.jpg",
+        medium: "../../thumbnails/off-the-track/regular/medium.jpg",
+        large: "../../thumbnails/off-the-track/regular/large.jpg",
       },
     },
     year: 2017,
@@ -349,9 +349,9 @@ export const moviesData = [
     title: "Whispering Hill",
     thumbnail: {
       regular: {
-        small: "../thumbnails/whispering-hill/regular/small.jpg",
-        medium: "../thumbnails/whispering-hill/regular/medium.jpg",
-        large: "../thumbnails/whispering-hill/regular/large.jpg",
+        small: "../../thumbnails/whispering-hill/regular/small.jpg",
+        medium: "../../thumbnails/whispering-hill/regular/medium.jpg",
+        large: "../../thumbnails/whispering-hill/regular/large.jpg",
       },
     },
     year: 2017,
@@ -365,9 +365,9 @@ export const moviesData = [
     title: "112",
     thumbnail: {
       regular: {
-        small: "../thumbnails/112/regular/small.jpg",
-        medium: "../thumbnails/112/regular/medium.jpg",
-        large: "../thumbnails/112/regular/large.jpg",
+        small: "../../thumbnails/112/regular/small.jpg",
+        medium: "../../thumbnails/112/regular/medium.jpg",
+        large: "../../thumbnails/112/regular/large.jpg",
       },
     },
     year: 2013,
@@ -381,9 +381,9 @@ export const moviesData = [
     title: "Lone Heart",
     thumbnail: {
       regular: {
-        small: "../thumbnails/lone-heart/regular/small.jpg",
-        medium: "../thumbnails/lone-heart/regular/medium.jpg",
-        large: "../thumbnails/lone-heart/regular/large.jpg",
+        small: "../../thumbnails/lone-heart/regular/small.jpg",
+        medium: "../../thumbnails/lone-heart/regular/medium.jpg",
+        large: "../../thumbnails/lone-heart/regular/large.jpg",
       },
     },
     year: 2017,
@@ -397,9 +397,9 @@ export const moviesData = [
     title: "Production Line",
     thumbnail: {
       regular: {
-        small: "../thumbnails/production-line/regular/small.jpg",
-        medium: "../thumbnails/production-line/regular/medium.jpg",
-        large: "../thumbnails/production-line/regular/large.jpg",
+        small: "../../thumbnails/production-line/regular/small.jpg",
+        medium: "../../thumbnails/production-line/regular/medium.jpg",
+        large: "../../thumbnails/production-line/regular/large.jpg",
       },
     },
     year: 2018,
@@ -413,9 +413,9 @@ export const moviesData = [
     title: "Dogs",
     thumbnail: {
       regular: {
-        small: "../thumbnails/dogs/regular/small.jpg",
-        medium: "../thumbnails/dogs/regular/medium.jpg",
-        large: "../thumbnails/dogs/regular/large.jpg",
+        small: "../../thumbnails/dogs/regular/small.jpg",
+        medium: "../../thumbnails/dogs/regular/medium.jpg",
+        large: "../../thumbnails/dogs/regular/large.jpg",
       },
     },
     year: 2016,
@@ -429,9 +429,9 @@ export const moviesData = [
     title: "Asia in 24 Days",
     thumbnail: {
       regular: {
-        small: "../thumbnails/asia-in-24-days/regular/small.jpg",
-        medium: "../thumbnails/asia-in-24-days/regular/medium.jpg",
-        large: "../thumbnails/asia-in-24-days/regular/large.jpg",
+        small: "../../thumbnails/asia-in-24-days/regular/small.jpg",
+        medium: "../../thumbnails/asia-in-24-days/regular/medium.jpg",
+        large: "../../thumbnails/asia-in-24-days/regular/large.jpg",
       },
     },
     year: 2020,
@@ -445,9 +445,9 @@ export const moviesData = [
     title: "The Tasty Tour",
     thumbnail: {
       regular: {
-        small: "../thumbnails/the-tasty-tour/regular/small.jpg",
-        medium: "../thumbnails/the-tasty-tour/regular/medium.jpg",
-        large: "../thumbnails/the-tasty-tour/regular/large.jpg",
+        small: "../../thumbnails/the-tasty-tour/regular/small.jpg",
+        medium: "../../thumbnails/the-tasty-tour/regular/medium.jpg",
+        large: "../../thumbnails/the-tasty-tour/regular/large.jpg",
       },
     },
     year: 2016,
@@ -461,9 +461,9 @@ export const moviesData = [
     title: "Darker",
     thumbnail: {
       regular: {
-        small: "../thumbnails/darker/regular/small.jpg",
-        medium: "../thumbnails/darker/regular/medium.jpg",
-        large: "../thumbnails/darker/regular/large.jpg",
+        small: "../../thumbnails/darker/regular/small.jpg",
+        medium: "../../thumbnails/darker/regular/medium.jpg",
+        large: "../../thumbnails/darker/regular/large.jpg",
       },
     },
     year: 2017,
@@ -477,9 +477,9 @@ export const moviesData = [
     title: "Unresolved Cases",
     thumbnail: {
       regular: {
-        small: "../thumbnails/unresolved-cases/regular/small.jpg",
-        medium: "../thumbnails/unresolved-cases/regular/medium.jpg",
-        large: "../thumbnails/unresolved-cases/regular/large.jpg",
+        small: "../../thumbnails/unresolved-cases/regular/small.jpg",
+        medium: "../../thumbnails/unresolved-cases/regular/medium.jpg",
+        large: "../../thumbnails/unresolved-cases/regular/large.jpg",
       },
     },
     year: 2018,
@@ -493,9 +493,9 @@ export const moviesData = [
     title: "Mission: Saturn",
     thumbnail: {
       regular: {
-        small: "../thumbnails/mission-saturn/regular/small.jpg",
-        medium: "../thumbnails/mission-saturn/regular/medium.jpg",
-        large: "../thumbnails/mission-saturn/regular/large.jpg",
+        small: "../../thumbnails/mission-saturn/regular/small.jpg",
+        medium: "../../thumbnails/mission-saturn/regular/medium.jpg",
+        large: "../../thumbnails/mission-saturn/regular/large.jpg",
       },
     },
     year: 2017,
